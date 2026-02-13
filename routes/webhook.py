577ca627323b7d0746d0de8168a298c57e0f96ec
Math1611 <mathiasrosales490@gmail.com>
2026-02-13 13:00:38 -0300

    w

--- routes/webhook.py
+++ routes/webhook.py
@@ -1,43 +1,26 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import PlainTextResponse
-from config import VERIFY_TOKEN
 from services.flow import handle_button, handle_text
-from database import db
-
 
 router = APIRouter()
 
 
-@router.get("/webhook")
-async def verify(request: Request):
-    params = request.query_params
-
-    if (
-        params.get("hub.mode") == "subscribe"
-        and params.get("hub.verify_token") == VERIFY_TOKEN
-    ):
-        return PlainTextResponse(params.get("hub.challenge"))
-
-    return {"error": "verification failed"}
-
-
 @router.post("/webhook")
 async def webhook(request: Request):
+
     data = await request.json()
 
-    try:
-        msg = data["entry"][0]["changes"][0]["value"]["messages"][0]
-        user = msg["from"]
+    message = data["message"]
 
-        if msg["type"] == "text":
-            text = msg["text"]["body"]
-            await handle_text(user, text,db)
+    phone = message["from"]
 
-        elif msg["type"] == "interactive":
-            button_id = msg["interactive"]["button_reply"]["id"]
-            await handle_button(user, button_id)
+    # texto
+    if message["type"] == "text":
+        text = message["text"]["body"]
+        await handle_text(phone, text)
 
-    except Exception as e:
-        print("ERROR:", e)
+    # botón
+    elif message["type"] == "button":
+        button_id = message["button"]["payload"]
+        await handle_button(phone, button_id)
 
-    return {"ok": True}
+    return {"status": "ok"}
