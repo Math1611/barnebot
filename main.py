b8f39a1616f0f02e54f40a6be6d1f5f44c3f5640
Math1611 <mathiasrosales490@gmail.com>
2026-02-16 10:29:27 -0300

    db_new

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from routes.webhook import router as webhook_router
 from database.db import Base, engine
-from database.db import engine, Base
-from database import models
 import os
 import uvicorn
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(webhook_router)
+
+@app.get("/")
+def root():
+    return {"status": "running"}
 
 if __name__ == "__main__":
     uvicorn.run(
@@ -15,16 +26,3 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8000))
     )
-
-
-Base.metadata.create_all(bind=engine)
-
-app.include_router(webhook_router)
-
-@app.get("/")
-def root():
-    return {"status": "running"}
-    
-@app.on_event("startup")
-def startup():
-    Base.metadata.create_all(bind=engine)
