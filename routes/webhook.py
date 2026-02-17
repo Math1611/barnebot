efc12eb94dca8ac7f3c0f8666a158bf7d40617bd
Math1611 <mathiasrosales490@gmail.com>
2026-02-17 10:55:22 -0300

    sd

--- routes/webhook.py
+++ routes/webhook.py
@@ -1,34 +1,12 @@
-from fastapi import APIRouter, Request
-from services.flow import handle_text, handle_button
+from fastapi import Request
 
-router = APIRouter()
-
-
-@router.post("/webhook")
+@app.post("/webhook")
 async def webhook(request: Request):
-
-    data = await request.json()
-
     try:
-        value = data["entry"][0]["changes"][0]["value"]
+        data = await request.json()
+    except:
+        data = {}
 
-        if "messages" not in value:
-            return {"status": "no messages"}
+    print("DATA RECIBIDA:", data)
 
-        message = value["messages"][0]
-        phone = message["from"]
-
-        if message["type"] == "text":
-            text = message["text"]["body"]
-            await handle_text(phone, text)
-
-
-        elif message["type"] == "interactive":
-            button_id = message["interactive"]["button_reply"]["id"]
-            await handle_button(phone, button_id)
-
-        return {"status": "ok"}
-
-    except Exception as e:
-        print("🔥 ERROR WEBHOOK:", e)
-        return {"status": "error"}
+    return {"status": "ok"}
