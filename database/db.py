58eb589bbf4f580ed879630cce6f108abd565fe0
Math1611 <mathiasrosales490@gmail.com>
2026-02-17 10:25:44 -0300

    render

--- database/db.py
+++ database/db.py
@@ -2,23 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🔹 Construir la URL correctamente con SSL
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
-)
-
-# 🔹 Crear engine
 engine = create_engine(DATABASE_URL)
 
-# 🔹 Crear sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 🔹 Base declarativa
 Base = declarative_base()
